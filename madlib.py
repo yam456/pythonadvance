a6266801fee85d90c8fee468bea9ec8166e17516
Yamini M R <yaminimr2@gmail.com>
2023-10-18 21:47:27 +0530

    first commit

---
+++ madlib.py
@@ -0,0 +1,10 @@
+adjective=input("Adjective: ")
+verb1=input("Verb: ")
+verb2=input("Verb: ")
+famouse_person=input("Famous person: ")
+
+madlib=f"Computer programming is so {adjective}! It makes me so excited all the time because 
+     I love to {verb1}.Stay hydrated and {verb2} like you are {famouse_person}!"
+print(madlib)
+
+
